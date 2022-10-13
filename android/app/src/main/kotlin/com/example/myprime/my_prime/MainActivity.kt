0fec99b9b85896d17aae155d32b4470427622c55
Anucha Seetasung <anucha@diversition.co.th>
2022-10-14 02:00:01 +0700

    Init Project

---
+++ android/app/src/main/kotlin/com/example/myprime/my_prime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myprime.my_prime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textEditingController = TextEditingController();
+  String result = 'Please input number.';
+
+  onCalculatePrime() async {
+    setState(() {
+      result = 'Calculating...';
+    });
+    try{
+      double number = double.parse(textEditingController.text);
+      if(await isPrime(number)){
+        result = 'The closest prime is $number';
+      } else {
+        result = await checkClosestPrime(number-1, number+1);
+      }
+    }catch(e){
+      result = e.toString();
+    }
+    setState(() {});
+  }
+
+  Future<String> checkClosestPrime(double lowerNumber, double upperNumber) async {
+    bool isLowerPrime = await isPrime(lowerNumber);
+    bool isUpperPrime = await isPrime(upperNumber);
+    if(isLowerPrime && isUpperPrime){
+      return 'The closest prime is $lowerNumber and $upperNumber';
+    }
+    if(isLowerPrime){
+      return 'The closest prime is $lowerNumber';
+    }
+    if(isUpperPrime){
+      return 'The closest prime is $upperNumber';
+    }
+    return await checkClosestPrime(lowerNumber-1, upperNumber+1);
+  }
+
+  Future<bool> isPrime(double number) async {
+    if(number == 2){
+      return true;
+    }
+    if(number < 2){
+      return false;
+    }
+    if(number%2==0){
+      return false;
+    }
+    for(int i = 3; i <= number~/i; i += 2){
+      if(number%i==0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(result),
+              TextField(
+                controller: textEditingController,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+              TextButton(onPressed: () => onCalculatePrime(), child: const Text('Calculate'))
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
